91c56a73e5000235682d5e11e7b8894d6cf2f074
BarbodH <habibi.k.barbod@gmail.com>
2025-10-09 13:15:10 -0400

    Set up mock database & initialize contacts

--- app-03-contact-manager/app/src/main/java/com/github/bitstachio/contactmanager/activities/ContactsListActivity.java
+++ app-03-contact-manager/app/src/main/java/com/github/bitstachio/contactmanager/activities/ContactsListActivity.java
@@ -9,8 +9,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.github.bitstachio.contactmanager.R;
+import com.github.bitstachio.contactmanager.db.MockDatabase;
+import com.github.bitstachio.contactmanager.model.Contact;
+
+import java.util.ArrayList;
 
 public class ContactsListActivity extends AppCompatActivity {
+
+    private ArrayList<Contact> contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,7 @@ public class ContactsListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        contacts = MockDatabase.getContacts();
     }
 }
--- app-03-contact-manager/app/src/main/java/com/github/bitstachio/contactmanager/model/Contact.java
+++ app-03-contact-manager/app/src/main/java/com/github/bitstachio/contactmanager/model/Contact.java
@@ -4,10 +4,10 @@ import lombok.Data;
 
 @Data
 public class Contact {
-    private String firstName;
-    private String lastName;
-    private String phone;
-    private String email;
-    private String birthday;
-    private String notes;
+    private final String firstName;
+    private final String lastName;
+    private final String phone;
+    private final String email;
+    private final String birthday;
+    private final String notes;
 }
