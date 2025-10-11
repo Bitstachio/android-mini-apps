68dfc670342a33ef18f3f696184cbe177d721af5
BarbodH <habibi.k.barbod@gmail.com>
2025-10-11 18:13:28 -0400

    Implement contact DAO with Room

---
+++ app-03-contact-manager/app/src/main/java/com/github/bitstachio/contactmanager/db/RoomContactDao.java
@@ -0,0 +1,29 @@
+package com.github.bitstachio.contactmanager.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.github.bitstachio.contactmanager.model.Contact;
+
+import java.util.List;
+
+@Dao
+public interface RoomContactDao extends IContactDao {
+    @Query("SELECT * FROM Contact")
+    List<Contact> getAll();
+
+    @Query("SELECT * FROM Contact WHERE id = :id")
+    Contact getById(int id);
+
+    @Insert
+    long insert(Contact contact);
+
+    @Update
+    int update(Contact contact);
+
+    @Delete
+    int delete(Contact contact);
+}
